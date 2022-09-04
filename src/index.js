2bfa481ae7e2dfedec48f93c9a98bbb9e0c63115
ganeodolu <ganeodolu@gmail.com>
2022-09-04 23:34:18 +0900

    feat: 할일 객체 정의 및 메소드 추가

--- src/index.js
+++ src/index.js
@@ -1,51 +1,76 @@
 /**
  * Represents a book.
  * @constructor
- * @param {string} title - The title of the book.
- * @param {string} author - The author of the book.
+ * @param {Todo[]} todolist - 할일 목록
  */
-function Book(title, author) {}
+function TodoApp(todolist) {
+  this.todolist = todolist;
+}
 
 /**
- * Both of these will link to the bar function.
- * @see {@link bar}
- * @see bar
+ * 할일 생성하기
+ * @param {Todo[]} todolist - 기존 할일 목록
+ * @param {Todo} newTodo - 새로운 할일
+ * @return {Todo[]} 새로운 할일 목록
  */
-function foo() {}
-
-// Use the inline {@link} tag to include a link within a free-form description.
-/**
- * @see {@link foo} for further information.
- * @see {@link http://github.com|GitHub}
- */
-function bar() {}
-
-/**
- * Generic dairy product.
- * @constructor
- */
-function DairyProduct() {}
-
-/**
- * Check whether the dairy product is solid at room temperature.
- * @abstract
- * @return {boolean}
- */
-DairyProduct.prototype.isSolid = function () {
-	throw new Error('must be implemented by subclass!');
+TodoApp.prototype.create = function (todolist, newTodo) {
+  this.todolist = [newTodo, ...this.todolist]
+  // return newTodoList
 };
 
 /**
- * Cool, refreshing milk.
- * @constructor
- * @augments DairyProduct
+ * 할일 조회하기
+ * @param {string} id - 아이디
+ * @return {Todo[]}
  */
-function Milk() {}
+ TodoApp.prototype.read = function (id) {};
+
+ /**
+ * 할일 수정하기
+ * @param {string} id - 아이디
+ * @return {Todo[]}
+ */
+  TodoApp.prototype.update = function (id) {};
+
+ /**
+ * 할일 삭제하기
+ * @param {string} id - 아이디
+ * @return {Todo[]}
+ */
+  TodoApp.prototype.delete = function (id) {};
 
 /**
- * Check whether milk is solid at room temperature.
- * @return {boolean} Always returns false.
+ * Todo 정의
+ * @typedef {object} Todo
+ * @property {string} id - 아이디
+ * @property {string} content - 내용
+ * @property {boolean} isCompleted - 완료여부
+ * @property {string} category - 카테고리
+ * @property {string[]} tags - 태그들
  */
-Milk.prototype.isSolid = function () {
-	return false;
-};
+
+// Todo {
+//   아이디(required),
+//   내용(required),
+//   완료여부(required),
+//   카테고리(required),
+//   태그들(optional),
+// }
+
+// CREATE
+// 할 일을 추가할 수 있다.
+// 내용없이 추가할 수 없다.
+
+// READ
+// 모든 할 일을 조회할 수 있다.
+// ID를 기반으로 특정 할 일을 조회할 수 있다.
+
+// UPDATE
+// ID를 제외한 모든 속성을 수정할 수 있다.
+// 특정 할 일의 특정 태그를 수정할 수 있다.
+
+// DELETE
+// ID를 기반으로 특정 할 일을 삭제할 수 있다.
+// 모든 할 일을 제거할 수 있다.
+// 특정 할 일의 특정 태그를 삭제할 수 있다.
+// 특정 할 일의 모든 태그를 제거할 수 있다.
